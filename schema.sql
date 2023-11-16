062f198bf3b837beeccac68c7412e9ea115fe90f
Malili05 <malisalitten@gmail.com>
2023-11-15 21:29:20 -0700

    added schema table setup

---
+++ schema.sql
@@ -0,0 +1,40 @@
+
+-- Initialize and Configure Database
+DROP DATABASE IF EXISTS employee_db;
+CREATE database employee_db;
+
+
+USE employee_db;
+
+
+CREATE TABLE department (
+
+
+    id INT NOT NULL AUTO_INCREMENT,
+    dept_name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+
+CREATE TABLE employees (
+
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT NOT NULL,
+    PRIMARY KEY (id)
+
+);
+
+
+create table roles (
+
+
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL,
+    PRIMARY KEY (id)
+
+);
